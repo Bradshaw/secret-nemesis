9b344caaf561134d1e5c1e426ca742790201020e
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-01-12 23:10:02 +0100

    Removed legacy expermiments, added a pretty sine scroller in the mean time...

--- server/secret.js
+++ server/secret.js
@@ -3,11 +3,6 @@ app = require('http').createServer(handler)
   , fs = require('fs');
 
 var repl = require('repl');
-
-say = function(s){
-  console.log('Saying "'+s+'"');
-}
-
 var rep = repl.start({
   prompt: "server> ",
   input: process.stdin,
@@ -34,7 +29,7 @@ function handler (req, res) {
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end(__dirname+ ' ' +filename);
+      return res.end('Mistakes were made...');
     }
 
     res.writeHead(200);
@@ -46,22 +41,6 @@ function clock(){
 }
 
 io.sockets.on('connection', function (socket) {
-  socket.set('pos', {x: 150, y:150});
-  socket.set('rect', {x1: 10, y1:10, x2: 20, y2:20});
-
-  // Update loop
-  setInterval(function(){
-    // Get the current position
-    socket.get('pos', function (err, data) {
-      // And send it to the client
-      socket.emit('move', data);
-    });
-    socket.get('rect', function (err, data) {
-      // And send it to the client
-      socket.emit('rect', data);
-    });
-  },50);
-
   /* Ping testing */
   // Every second
   setInterval(function(){
@@ -76,38 +55,6 @@ io.sockets.on('connection', function (socket) {
     socket.get('pingstart', function (err, data) {
       // Set the new ping to new time
       socket.set('ping',clock()-data);
-      // Inform the client of his current ping
-      socket.get('ping', function (err, data) {
-        socket.emit('currentPing', {ping: data});
-      });
     });
   });
-
-
-  socket.on('click', function (data) {
-    // Update our memory of it
-  });
-
-  socket.on('realclick', function (data) {
-    // Update our memory of it
-    socket.set('pos', {x: data.x, y:data.y});
-  });
-
-  // When the client informs us of his new position
-  socket.on('mousedown', function (data) {
-    // Update our memory of it
-  });
-  
-  socket.on('mouseup', function (data) {
-    // Update our memory of it
-  });
-
-
-  socket.on('selectybox', function (data) {
-    socket.set('rect', data);
-  });
-
-
-
-
 });
