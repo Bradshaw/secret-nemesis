74056a6d47cce4c256cf9fea3bf59118362af53b
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-01-13 12:03:05 +0100

    Added gamestate manager
    Need a gamestate to test with, but for that, I need a game loop working. Next
    step?

---
+++ client/modules/gamestate.js
@@ -0,0 +1,133 @@
+var gs = {};
+gs.current = null;
+gs.next = null;
+gs.switchargs = [];
+
+
+/**	Switch to new gamestate on next update
+*
+*	Accepts an arbitrary number of arguments and passes them as an array to the
+*	next state.
+*/
+gs.switchstate = function(nextstate) { // Nextstate needs to be a gamestate
+	// Set the next state
+	gs.next = nextstate;
+	// Create an array to store extra arguments
+	gs.switchargs = [];
+	// Copy extra arguments to array
+	for (var i = 1; i < arguments.length; i++) {
+		gs.switchargs[i-1] = arguments[i];
+	};
+}
+
+/**	Updates the gamestate manager
+*
+*	If a next state is defined, switch to that state. Then update the current
+*	state
+*/
+gs.update = function() {
+	// If there's a state to switch to
+	if (gs.next) {
+		// If there's a current state
+		if (gs.current) {
+			// Run the leave() method so it can clean up
+			gs.current.leave();
+		}
+
+		// If the next state hasn't been initialised yet
+		if (!gs.next.initialised) {
+			// Initialise it
+			gs.next.initialised = true;
+			gs.next.init();
+		}
+
+		// Switch to the state
+		gs.current = gs.next;
+		gs.next = null;
+
+		// Run the enter method so it can prepare itself
+		gs.current.enter(gs.switchargs);
+		gs.switchargs = [];
+	}
+
+	// Make sure we have a state to update
+	if (gs.current){
+		// Update it
+		gs.current.update();
+	} else {
+		// Otherwise display a friendly reminder
+		console.log('No current gamestate defined!');
+		console.log('Forgot to gs.switchstate(state) maybe?');
+	}
+}
+
+/**	Creates a new state
+*
+*	Reminder: Call this one like:
+*		var myState = new gs.gamestate('myState');
+*		              ^^^
+*	otherwise you'll feel silly...
+*/
+gs.gamestate = function(name) {
+	// Set the attributes
+	this.name = name;
+	this.initialised = false;
+}
+
+/* Default methods */
+// These guys don't really do anything.
+// They should be redefined for each state by the framework user
+
+/**	Initialises the state
+*
+*	Called once, when the state is first used. Initialise all the objects the 
+*	state will need in here
+*/
+gs.gamestate.prototype.init = function() {};
+
+/**	Prepares the state for entry
+*
+*	Called each time the state is entered.
+*	Useful to prepare the environment.
+*/
+gs.gamestate.prototype.enter = function(previous) {};
+
+/**	Prepare the state for departure
+*
+*	Called each time we switch away from the state.
+*	Allows the state to clean up after itself.
+*	Be a friendly state and leave things how they were when you found them!
+*/
+gs.gamestate.prototype.leave = function() {};
+
+/** Called each frame
+*
+*	The contents of your main loop goes here.
+*/
+gs.gamestate.prototype.update = function() {};
+
+/**	Called when keys are pressed
+*
+*	key - name of the pressed key
+*/
+gs.gamestate.prototype.keypressed = function(key) {};
+
+/**	Called when keys are released
+*
+*	key - name of the released key
+*/
+gs.gamestate.prototype.keyreleased = function(key) {};
+
+/**	Called when mouse buttons are pressed
+*
+*	btn - number of the mouse button
+*	x,y - coordinates where the event occured
+*/
+gs.gamestate.prototype.mousepressed = function(btn, x, y) {};
+
+/**	Called when mouse buttons are released
+*
+*	btn - number of the mouse button
+*	x,y - coordinates where the event occured
+*/
+gs.gamestate.prototype.mousereleased = function(btn, x, y) {};
